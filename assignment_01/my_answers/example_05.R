8037f9c80b1014eed5fba1ccb2679475a3ddb366
Victoria Byerley <54579088+vbyerley@users.noreply.github.com>
2019-09-12 11:15:22 -0400

    Update example_05.R

--- assignment_01/my_answers/example_05.R
+++ assignment_01/my_answers/example_05.R
@@ -1,1 +1,11 @@
-# Write an R function that will count all the event numbers up to a user defined stopping point.
+# Write an R function that will count all the even numbers up to a user defined stopping point.
+
+# Get number from user.
+upper_limit <- as.integer(readline(prompt="At what number would you like to stop?: "))
+
+x <- 2
+
+while (x <= upper_limit){
+  print(x)
+  x <- x + 2
+}
