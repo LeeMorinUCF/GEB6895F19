b1ce42cb9b4701656c2cc63cc321ca4c45c6b426
Victoria Byerley <54579088+vbyerley@users.noreply.github.com>
2019-09-11 21:02:06 -0400

    Create example_06.R

---
+++ assignment_01/my_answers/example_06.R
@@ -0,0 +1,5 @@
+# Write an R function that will perform the following
+# a) Read in 5 separate numbers
+# b) Calculate the average of the five numbers
+# c) Find the smallest (minimum) and largest (maximum) of the five entered numbers
+# d) Write out the results found from steps b and c with a message describing the results, i.e. maximum is ....
