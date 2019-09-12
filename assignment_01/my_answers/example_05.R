763c590900b12521f8770cabba74679f78492a33
Victoria Byerley <54579088+vbyerley@users.noreply.github.com>
2019-09-12 11:47:11 -0400

    Update example_05.R

--- assignment_01/my_answers/example_05.R
+++ assignment_01/my_answers/example_05.R
@@ -3,8 +3,10 @@
 # Get number from user.
 upper_limit <- as.integer(readline(prompt="At what number would you like to stop?: "))
 
+# Set x to 2 (so 0 doesn't populate in answer)
 x <- 2
 
+# Loop to give multiples of two until it hits the upper limit
 while (x <= upper_limit){
   print(x)
   x <- x + 2
