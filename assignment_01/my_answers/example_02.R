ecdbd9d4c4bac6c08e52cbf04e26c11bc40baf35
Victoria Byerley <54579088+vbyerley@users.noreply.github.com>
2019-09-12 08:43:52 -0400

    Update example_02.R

--- assignment_01/my_answers/example_02.R
+++ assignment_01/my_answers/example_02.R
@@ -1,2 +1,19 @@
 # Write an R function that tells a user that the number they entered is not a 5 or a 6.
 # You can read input from the keyboard with num_in <- readline(prompt="Enter a number:  ")
+
+
+
+# Get number from user.
+num_in <- readline(prompt="Enter a number: ")
+
+# Determine if number is a 5 or 6.
+if (num_in == '5') {
+  print('You entered a 5.')
+  
+} else if (num_in == '6') {
+  print('You entered a 6.')
+  
+} else {
+  print('You did not enter a 5 or a 6')
+  
+} 
