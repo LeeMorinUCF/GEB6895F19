2e68828400c71347b193224c9133bcee55b88b17
Courtney Dickson <courtneypdickson@knights.ucf.edu>
2019-09-25 20:50:01 -0400

    Revert "assignment answers"
    This reverts commit c02fc7c4fea2539871c2f0215be0ab9b652d2d7c.
    
    My comment goes here.

--- assignment_02/Problem 4.R
+++
@@ -1,6 +0,0 @@
-num_in <- readline(prompt = "enter a number:")
-
-num_in <- as.integer(num_in)
-
-input <- seq(1, num_in, by = 5)
-  print(input)
--- assignment_02/Problem 5.R
+++
@@ -1,9 +0,0 @@
-num_in <- readline(prompt = "enter a number:")
-
-num_in <- as.integer(num_in)
-
-input <- seq(0, num_in, by = 2)
-
-  sum_even_number <- length(input)
-  
-  print(sum_even_number)
