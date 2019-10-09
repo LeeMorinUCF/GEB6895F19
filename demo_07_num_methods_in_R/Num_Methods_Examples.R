29bbfaae7663e38133552f40afb4ec8a0b5c86b1
Lee Morin <Lealand.Morin@ucf.edu>
2019-10-09 17:38:34 -0400

    Adjustments to numerical methods

--- demo_07_num_methods_in_R/Num_Methods_Examples.R
+++ demo_07_num_methods_in_R/Num_Methods_Examples.R
@@ -289,8 +289,11 @@ grr <- function(x) { ## Gradient of 'fr'
   c(-400 * x1 * (x2 - x1 * x1) - 2 * (1 - x1),
     200 *      (x2 - x1 * x1))
 }
+
 optim(c(-1.2,1), fr)
-(res <- optim(c(-1.2,1), fr, grr, method = "BFGS"))
+res <- optim(c(-1.2,1), fr, grr, method = "BFGS")
+
+# Compare with other algorithms:
 optimHess(res$par, fr, grr)
 optim(c(-1.2,1), fr, NULL, method = "BFGS", hessian = TRUE)
 ## These do not converge in the default number of steps
